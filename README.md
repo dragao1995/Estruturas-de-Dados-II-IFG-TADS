53629e87c1f94d78d350d5aa2a41ad4a191746a2
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-03-09 11:32:08 -0300

    Adicionando Contribuições em Java - Quicksort

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A compilação de qualquer arquivo fonte .cpp pode ser feita a partir da linha d
 ```shell
 $g++ arquivofonte.cpp -std=c++11 -o nomedoexecutavel
 ```
+## Contribuidores
+* Alex Sandro da Silva Magalhães Júnior
+** Quicksort em Java
+
--- gera-grafico.py
+++ gera-grafico.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(n):
-    return 2*np.power(n,2)
+    return n
 
 def g(n):
-	return np.power(n,2)+n	
+	return n-n+50
 
 #define intervalo e amostragem
-amostra = np.arange(0, 1000, 0.5)
+amostra = np.arange(0, 200, 0.5)
 
 plt.plot(amostra, f(amostra), 'b-',label="f(n)")
-plt.plot(amostra, g(amostra), 'r--',label="g(n)")
+plt.plot(amostra, g(amostra) , 'r--',label="g(n)")
 plt.xlabel("n")
 plt.ylabel("Passos")
 plt.title("Comportamento de Funções") 
