7c4fabc6b1b733fa3bb74eb7e2900ff906d09855
dragao1995 <dragao1995@gmail.com>
2016-03-16 21:16:12 -0300

    Quicksort.java em List

--- Ordenação/Quicksort.java
+++ Ordenação/Quicksort.java
@@ -1,31 +1,37 @@
+package ordenacao;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Quicksort {
-	public static void main(String[] args) {	   
+public class quicksort {
+	public static void main(String[] args) {
+
+	   
 	    int n = 1000;
 	    Random rng = new Random();// random-number engine used (Mersenne-Twister in this case)
-	    int[] v = new int[1000];
+	    List<Integer> v = new ArrayList<Integer>();
 	    for(int i=0;i<n;i++){
-	        v[i] = (rng.nextInt(1000));//gera de 0 a 999
+	        v.add(rng.nextInt(1000));//gera de 0 a 999
 	    }
-	    System.out.println("Imprimindo vetor antes da ordenaÃ§Ã£o.\n");
+	    System.out.println("Imprimindo vetor antes da ordenação.\n");
 	    for(int i=0;i<n;i++){
-	       System.out.println("v["+i+"] = "+v[i]);
+	       System.out.println("v["+i+"] = "+v.get(i));
 	    }
 	    System.out.println("\nQuicksort!\n");
 	    quicksort(v);
-	    System.out.println("Imprimindo vetor depois da ordenaÃ§Ã£o.\n");
+	    System.out.println("Imprimindo vetor depois da ordenação.\n");
 	    for(int i=0;i<n;i++){
-	    	 System.out.println("v["+i+"] = "+v[i]);
+	    	 System.out.println("v["+i+"] = "+v.get(i));
 	    }
 	}
 
-	private static void quicksort(int[] v) {
-		 recursive_quicksort(v,0,v.length-1);
+	private static void quicksort(List<Integer> v) {
+		 recursive_quicksort(v,0,v.size()-1);
 		
 	}
 
-	private static void recursive_quicksort(int[] v, int l, int r) {
+	private static void recursive_quicksort(List<Integer> v, int l, int r) {
 		 if(l<r){
 		        int p = partition(v,l,r);
 		        recursive_quicksort(v,l,p-1);
@@ -34,23 +40,25 @@ public class Quicksort {
 		
 	}
 
-	private static int partition(int[] v, int l, int r) {
+	private static int partition(List<Integer> v, int l, int r) {
 
-	    int k = l; //marca o fim da primeira partiÃ§Ã£o
+	    int k = l; //marca o fim da primeira partição
 	    int i = l; //elemento a ser analizado
-	    int pivo = r; //pivo Ã© o elemento do final
+	    int pivo = r; //pivo é o elemento do final
 	    while(i<r){
-	        if(v[i]<=v[pivo]){
-	        	int t = v[k];
-	  	      v[k] = v[i];
-	  	      v[i] = t;
+	        if(v.get(i)<=v.get(pivo)){
+	          int t = v.get(k);
+	  	      v.set(k, v.get(i));
+	  	      v.set(i,t);
 	            k++;
 	        }
 	        i++;
 	    }
-	    int t = v[k];
-	      v[k] = v[i];
-	      v[i] = t; //colocamos o pivo na posiÃ§Ã£o correta
-	    return k; //retornamos a posiÃ§Ã£o do pivo
-	}	
+	    int t = v.get(k);
+	      v.set(k, v.get(i));
+	      v.set(i,t);//colocamos o pivo na posição correta
+	    return k; //retornamos a posição do pivo
+	}
+
+	
 }
