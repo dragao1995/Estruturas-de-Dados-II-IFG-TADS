fbd33c2d3fec54a4138d5df55560fbf61d74ec42
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-06-14 10:42:36 -0300

    Adicionando exemplo de busca em largura

--- Grafos/exemplo-busca-em-largura-lista-de-adjacencias.cpp
+++ Grafos/exemplo-busca-em-largura-lista-de-adjacencias.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <limits>
 using namespace std;
 /**
  * A lista de adjacências representa o grafo em espaço O(|V|+|E|)
@@ -8,22 +9,29 @@ using namespace std;
  */
 
 void bfs(vector<vector<int>>& g, int v){
-    const int white = 0;
-    const int grey = 1;
-    const int black = 2;
-    vector<int> color(g.size(),white);
+    const int white = 0; //vértice não processado
+    const int grey = 1; //vértice em processamento
+    const int black = 2; // vértice processado
+    vector<int> color(g.size(),white); //cor
+    vector<int> d(g.size(),numeric_limits<int>::max()); //informação de distância
+    vector<int> pai(g.size(),-1); //informação do pai
     color[v]=grey;
+    d[v] = 0;
     queue<int> q;
     q.push(v);
     while(!q.empty()){
-        int u = q.front();
+        int u = q.front(); // retira o nó da fila
         cout << "Nó visitado = " << u << endl;
+        cout << "Pai do nó = " << pai[u] << endl;
+        cout << "Distância da origem " << v << " = " << d[u] << endl;
         q.pop();
-        for(size_t j=0;j<g[u].size();j++){
-            int w = g[u][j];
-            if(color[w]==white){
-                color[w] = grey;
-                q.push(w);
+        for(size_t j=0;j<g[u].size();j++){ //percorre a lista de adjacências
+            int w = g[u][j]; //retira o nó adjacente
+            if(color[w]==white){ //se o nó ainda não foi visitado, inclue ele na lista
+                color[w] = grey; // marca o nó como em processamento
+                pai[w] = u; //atualiza o pai
+                d[w] = d[u]+1; //atualiza a distância
+                q.push(w); //coloca o nó na fila
             }
         }
         color[u] = black;       
